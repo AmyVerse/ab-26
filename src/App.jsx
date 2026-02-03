a800b7bc89f7b97d39dc0559bce865f70513f155
Satvik Rastogi <satvikrastogi077@gmail.com>
2026-02-03 19:49:28 +0530

    adding theme component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Product from "./components/Product"
+import AboutTheme from "./components/AboutTheme"
+
+function App() {
+
+  return (
+    <>
+      {/* <h1
+        className="text-3xl font-bold underline text-cyan-500"
+      >
+        Hello Vite + React!
+      </h1> */}
+      <AboutTheme />
+
+      {/* Razorpahy test: For demo of working */}
+      {/* <Product/>   */}
+    </>
+  )
+}
+
+export default App
