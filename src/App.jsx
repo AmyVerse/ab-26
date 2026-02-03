fd43176c02d6debe455d5d5f54413342053a1954
Arnab Mistry <88276146+ArnabMistry@users.noreply.github.com>
2026-02-03 20:12:30 +0530

    feat(team): finalize responsive team section layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 
 //import Product from "./components/Product"
 import Sponsors from "./pages/Sponsors";
+import Teams from "./pages/Teams";
 
 
 function App() {
@@ -21,6 +22,9 @@ function App() {
 
       {/* Sponsors route */}
       <Route path="/sponsors" element={<Sponsors />} />
+      {/* Teams route */}
+      <Route path="/teams" element={<Teams />} />
+      
 
       {/* Razorpay demo route (optional for later) */}
       {/* <Route path="/payment-test" element={<Product />} /> */}
---
+++ src/pages/Teams.jsx
@@ -0,0 +1,13 @@
+// src/pages/Teams.jsx
+
+import TeamSection from "../components/team/TeamSection";
+
+const Teams = () => {
+  return (
+    <main>
+      <TeamSection />
+    </main>
+  );
+};
+
+export default Teams;
